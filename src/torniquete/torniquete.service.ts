6e71fae0286c74add974fba516d6ff0ad2517dd2
Kevin Mondragon <021000615@upsrj.edu.mx>
2024-10-29 19:27:15 -0600

    Feature: endpoint para mandar el estudiante o el empleado para el torniquete, en su modulo

--- src/torniquete/torniquete.service.ts
+++ src/torniquete/torniquete.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { StudentsService } from 'src/students/students.service';
 import { EmployeesService } from 'src/employees/employees.service';
+import { Employee } from '../employees/entities/employee.entity';
 
 @Injectable()
 export class TorniqueteService {
@@ -11,18 +12,20 @@ export class TorniqueteService {
     private readonly employeeService:EmployeesService
   ){}
 
-  async findOne(folio: string): Promise<boolean> {
+  async findOne(folio: string){
     try {
       // Intenta buscar en la base de datos de estudiantes
-      await this.studentsService.findOne(folio);
-      return true; // Si encuentra un estudiante
+      const {NombreCompleto , Carrera , images } = await this.studentsService.findOne(folio);
+      return {NombreCompleto , Carrera , images };
+    
     } catch (error) {
       // Si no se encuentra en estudiantes, ignora el error
     }
     try {
       // Intenta buscar en la base de datos de empleados
-      await this.employeeService.findOne(folio);
-      return true; 
+      const {NumeroDeEmpleado , Nombre , ApellidoPaterno , ApellidoMaterno , images} = await this.employeeService.findOne(folio); 
+      return { NumeroDeEmpleado , Nombre , ApellidoPaterno , ApellidoMaterno , images};
+      
     } catch (error) {
       throw new NotFoundException(`No student or employee found with folio "${folio}"`);// Si no se encuentra en empleados, ignora el error
     }
